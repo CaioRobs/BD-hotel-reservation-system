7c693454320dcaa51129659e32ab07e7732964c5
Caio Vieira <caio.robs98@gmail.com>
2024-10-19 14:26:16 -0300

    chore: testing spark analytics

--- spark-analytics/reservation_analytics.py
+++ spark-analytics/reservation_analytics.py
@@ -1,38 +1,34 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import (
-    StructType, 
-    StringType, 
-    IntegerType, 
+    StructType,
+    StringType,
+    IntegerType,
     TimestampType
 )
 
-# Initialize SparkSession
 spark = SparkSession.builder \
     .appName("ReservationAnalytics") \
     .getOrCreate()
 
-# Define Kafka parameters
 kafka_bootstrap_servers = "kafka:9092"
 reservation_topic = "reservation-events"
 
-# Define schema for reservation events
 reservation_schema = StructType() \
-    .add("eventType", StringType()) \
-    .add("reservation", StructType()
-         .add("roomId", IntegerType())
-         .add("userId", IntegerType())
-         .add("startDate", TimestampType())
-         .add("endDate", TimestampType()))
+    .add("type", StringType()) \
+    .add("roomId", IntegerType()) \
+    .add("roomType", IntegerType()) \
+    .add("floor", IntegerType()) \
+    .add("startDate", TimestampType()) \
+    .add("endDate", TimestampType()) \
+    .add("timestamp", IntegerType())
 
-# Read data from Kafka topic
 reservation_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
     .option("subscribe", reservation_topic) \
     .load()
 
-# Convert binary "value" column to string and parse JSON
 reservation_df = reservation_df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), reservation_schema).alias("data")) \
     .select("data.*")
@@ -42,5 +38,4 @@ query = reservation_df.writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
-
 query.awaitTermination()
